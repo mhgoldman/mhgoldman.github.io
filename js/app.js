f0a8dc00997ca2a0bf3399d04df7b67c400b2124
Martin Goldman <mgoldman@skytap.com>
2016-09-11 17:53:31 -0400

    scrolling works

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-// Scrolling anchor links
+var anchorPositions = [];
 
 $(function() {
-  $('a[href*="#"]').click(function(){
+  $('nav li a').click(function(){
     href = $.attr(this, 'href');
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
@@ -13,42 +13,40 @@ $(function() {
   });
 
 
-  var aChildren = $("nav li").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
-    for (var i=0; i < aChildren.length; i++) {    
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
+  var aChildren = $("nav li").children();
+  for (var i=0; i < aChildren.length; i++) {    
+    var aChild = aChildren[i];
+    var ahref = $(aChild).attr('href');
+    anchorPositions.push({id: ahref, position: $(ahref).offset().top});
+  }
 
-    $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
+  $(window).scroll(selectAnchor);
 
-        for (var i=0; i < aArray.length; i++) {
-            var anchorName = aArray[i];
-            var sectionID = anchorName + '-section';
-            var divPos = $(sectionID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(sectionID).height(); // get the height of the div in question
-
-            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                $("a[href='" + anchorName + "']").addClass("nav-active");
-            } else {
-                $("a[href='" + anchorName + "']").removeClass("nav-active");
-            }
-        }
-
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("nav-active")) {
-                var navActiveCurrent = $(".nav-active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                $("nav li:last-child a").addClass("nav-active");
-            }
-        }
-    });
-
+  selectAnchor();
 });
 
+function selectAnchor() {
+  var windowPos = $(window).scrollTop(); 
 
+  var selectedAnchor;
 
+  if (windowPos == 0) {
+    selectedAnchor = anchorPositions[0].id;
+  } else if (windowPos + $(window).height() >= $(document).height()) {
+    selectedAnchor = anchorPositions[anchorPositions.length-1].id;
+  } else {
+    for (i=0; i < anchorPositions.length; i++) {
+      if (windowPos >= anchorPositions[i].position) {
+        if (i >= anchorPositions.length-1 || windowPos < anchorPositions[i+1].position) {
+          selectedAnchor = anchorPositions[i].id;
+          break;
+        }
+      }
+    }
+  }
+
+  if (selectedAnchor) {
+    $("nav li a").removeClass("nav-active");
+    $("nav li a[href='" + selectedAnchor + "']").addClass("nav-active");      
+  }    
+}
